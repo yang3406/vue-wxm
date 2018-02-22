9117b732f4297d8bd0a89f450f2d98910264a5a2
yang3406 <1158890486@qq.com>
2018-02-22 16:43:57 +0800

    首页布局

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,88 +1,144 @@
 <template>
-    <div>
-      <div v-if="showMenu" >
-        <!--图片切换-->
-         <nav class="home-nav">
-             <div class="swiper-container" v-if="swiperImgList.length">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide" v-for="(item,index) in swiperImgList" :key="index">
-                      <img :src="item.imageUrl">
-                    </div>
-                </div>
-               <div class="swiper-pagination"></div>
-             </div>
-         </nav>
-         <section class="menu-section">
-           <div class="menu">
-             <ul class="outter-ul">
-               <li class="outter-li" v-for="(item,index) in tabMenuList" :key="index">
-                 <ul>
-                   <li><img :src="item.imageUrl"></img></li>
-                   <li>{{item.name}}</li>
-                 </ul>
-               </li>
-             </ul>
-           </div>
-         </section>
-      </div>
-
-      <div v-else>
-         <div></div>
-      </div>
-
-      <div class="weui-tabbar">
-        <a href="#tab1" class="weui-tabbar__item" :class="{active:isActive}" @click="toggleTab(event)">
-          <div class="weui-tabbar__icon">
-            <span class="service"></span>
+  <div>
+    <div v-if="showMenu">
+      <!--图片切换-->
+      <nav class="home-nav">
+        <div class="swiper-container" v-if="swiperImgList.length">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="(item,index) in swiperImgList" :key="index">
+              <img :src="item.imageUrl">
+            </div>
           </div>
-          <p class="weui-tabbar__label">服务</p>
-        </a>
-        <a href="#tab2" class="weui-tabbar__item" :class="{active:!isActive}" @click="toggleTab(event)">
-          <div class="weui-tabbar__icon">
-            <span class="self"></span>
-          </div>
-          <p class="weui-tabbar__label">我的</p>
-        </a>
-      </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </nav>
+      <section class="menu-section">
+        <div class="menu">
+          <ul class="outter-ul">
+            <li class="outter-li" v-for="(item,index) in tabMenuList" :key="index">
+              <ul>
+                <li><img :src="item.imageUrl"></img></li>
+                <li>{{item.name}}</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
+
+    <div v-else>
+      <form v-if="loginWay">
+        <div class="loginout">
+          <header class="user-login-text">
+            <h1>用户管理</h1>
+          </header>
+
+          <div class="user-login-container ">
+            <table class="table-query-deatail">
+              <tr>
+                <th>用户名</th>
+                <td name="username"></td>
+              </tr>
+              <tr>
+                <th>所属公司</th>
+                <a>
+                  <td name="companyName"></td>
+                </a>
+              </tr>
+            </table>
+            <button type="submit" id="loginoutuser"><a href="javascript:void(0);">退出登录</a></button>
+          </div>
+        </div>
+      </form>
+
+      <form v-else>
+        <div class="loginin">
+          <header class="user-login-text">
+            <h1>用户登录</h1>
+          </header>
+          <div class="user-login-container">
+            <table class="table-query-deatail">
+              <tr>
+                <th>用户名</th>
+                <td><input placeholder="请输入用户名" name="username" class="placeholder"/></td>
+              </tr>
+              <tr>
+                <th>密码</th>
+                <td><input placeholder="请输入密码" type="password" name="password" class="placeholder"/></td>
+              </tr>
+            </table>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <button type="submit" id="logininuser"><a href="javascript:void(0);">登录</a></button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div class="weui-tabbar">
+      <a href="#tab1" class="weui-tabbar__item" :class="{active:isActive}" @click="toggleTab('service',$event)">
+        <div class="weui-tabbar__icon">
+          <span class="service"></span>
+        </div>
+        <p class="weui-tabbar__label">服务</p>
+      </a>
+      <a href="#tab2" class="weui-tabbar__item" :class="{active:!isActive}" @click="toggleTab('owner',$event)">
+        <div class="weui-tabbar__icon">
+          <span class="self"></span>
+        </div>
+        <p class="weui-tabbar__label">我的</p>
+      </a>
+    </div>
+  </div>
 
 </template>
 
 <script>
-import {mapMutations} from 'vuex'
-import {tabMenuList,swiperImgList} from 'src/config/tabmenu.js'
-import 'src/plugins/swiper.min.js'
-import 'src/style/swiper.min.css'
+  import {mapMutations} from 'vuex'
+  import {tabMenuList, swiperImgList} from 'src/config/tabmenu.js'
+  import 'src/plugins/swiper.min.js'
+  import 'src/style/swiper.min.css'
 
-export default {
-  data(){
-    return {
-      showMenu:true,
-      isActive:true,
-      swiperImgList:swiperImgList,
-      tabMenuList
+  export default {
+    data() {
+      return {
+        showMenu: true,
+        isActive: true,
+        swiperImgList: swiperImgList,
+        tabMenuList,
+        loginWay: false
+      }
+    },
+    mounted() {
+      //初始化swiper
+      new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 2000
+      });
+    },
+    methods: {
+      toggleTab: function (param, event) {
+        event.preventDefault();
+        if (param == "service") {
+          this.isActive = true;
+          this.showMenu = true;
+        } else if (param == 'owner') {
+          this.isActive = false;
+          this.showMenu = false;
+        }
+      }
     }
-  },
-  mounted(){
-    //初始化swiper
-    new Swiper('.swiper-container', {
-      pagination: '.swiper-pagination',
-      loop: true,
-      autoplay:2000
-    });
-  },
-  method(){
-
-
-   }
-}
+  }
 
 </script>
 
 <style lang="css" scoped>
   @import "../../style/queryIndex.css";
 
-  .weui-tabbar{
+  .weui-tabbar {
     display: -webkit-flex;
     display: -webkit-box;
     display: flex;
@@ -92,18 +148,20 @@ export default {
     width: 100%;
     background: #f7f7fa;
   }
-  .weui-tabbar:before{
+
+  .weui-tabbar:before {
     position: absolute;
     content: "";
     width: 0;
     height: 1px;
     left: 0;
-    top:0;
+    top: 0;
     right: 0;
-    border-top:1px solid #c0bfc4;
-    color:#c0bfc4;
+    border-top: 1px solid #c0bfc4;
+    color: #c0bfc4;
   }
-  .weui-tabbar__item{
+
+  .weui-tabbar__item {
     position: relative;
     display: block;
     -webkit-box-flex: 1;
@@ -113,7 +171,7 @@ export default {
     font-size: 0;
     color: #999;
     text-align: center;
-    -webkit-tap-highlight-color: rgba(0,0,0,0)
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0)
   }
 
   .weui-tabbar__icon {
@@ -122,11 +180,11 @@ export default {
     height: 27px;
   }
 
-  .weui-tabbar .active{
-    color:#f48c2b;
+  .weui-tabbar .active {
+    color: #f48c2b;
   }
 
-  .menu-section{
+  .menu-section {
     margin-bottom: 53px;
     background-color: white;
   }
@@ -138,13 +196,12 @@ export default {
     line-height: 1.8;
   }
 
-  .swiper-slide img{
+  .swiper-slide img {
     height: 18rem;
     width: 100%;
   }
 
-
-  .weui-tabbar__icon .service{
+  .weui-tabbar .service {
     display: inline-block;
     background: url("../../assets/images/service_gray.png") no-repeat no-repeat center center;
     background-size: 100% 100%;
@@ -152,7 +209,7 @@ export default {
     width: 100%;
   }
 
-  .weui-tabbar__item.weui-bar__item--on .weui-tabbar__icon .service{
+  .weui-tabbar .active .service {
     display: inline-block;
     background: url("../../assets/images/service_yellow.png") no-repeat no-repeat center center;
     background-size: 100% 100%;
@@ -160,7 +217,7 @@ export default {
     width: 100%;
   }
 
-  .weui-tabbar__icon .self{
+  .weui-tabbar .self {
     display: inline-block;
     background: url("../../assets/images/self_gray.png") no-repeat no-repeat center center;
     background-size: 100% 100%;
@@ -168,7 +225,7 @@ export default {
     width: 100%;
   }
 
-  .weui-tabbar__item.weui-bar__item--on .weui-tabbar__icon .self{
+  .weui-tabbar .active .self {
     display: inline-block;
     background: url("../../assets/images/self_yellow.png") no-repeat no-repeat center center;
     background-size: 100% 100%;
