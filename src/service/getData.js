c7b3d9d236db67c9b675fa52060dd0eecbb98a09
yang3406 <1158890486@qq.com>
2018-03-21 20:07:52 +0800

    action

--- src/service/getData.js
+++ src/service/getData.js
@@ -8,9 +8,20 @@ import qs from 'qs'
 //获取用户数据
 //export  const  getHsCode = () => fetch('/hscode/app_query','GET', {size:7,pageNo:1}, 'fetch')
 
+//axios 全局配置
+axios.defaults.timeout = 5000;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.baseURL = baseDataUrl;
+/*axios.defaults = {
+  baseUrl:baseDataUrl,
+  timeout:5000,
+
+}*/
+
+
 //获取用户openID
-export const getOpenId = () => axios.post(wechatUrl+'getRoot/getWxmpuser')
+export const getOpenId = () => axios.post(wechatUrl+'/getRoot/getWxmpuser')
 //获取HS编码数据
 //export const getHsCode = (param) => axios.post(baseDataUrl+'hscode/app_query',{size:7,pageNo:1})
-export const getHsCode = (param) => axios({method:'post',url:baseDataUrl+'hscode/app_query',data:qs.stringify(param)})
+export const getHsCode = (param) => axios.post('/hscode/app_query',param)
 
